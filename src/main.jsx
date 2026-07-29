0917f9fb864c164ab9be9aaf279437f5c4e170da
Brightsteps2026 <brightstepsinterschool@gmail.com>
2026-07-29 23:42:04 +0000

    Wrap App component with LoginGate

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import "./lib/windowStorage";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import LoginGate from "./LoginGate.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <LoginGate>
+      <App />
+    </LoginGate>
   </React.StrictMode>
 );
